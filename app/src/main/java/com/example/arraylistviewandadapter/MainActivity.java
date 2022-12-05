0824b9e430fe10b224d67142b92d8a882c5a2702
Hassan001 <bitf19a001@pucit.edu.pk>
2022-12-05 22:57:58 +0500

    Initialized array list and binded with array adapter

--- app/src/main/java/com/example/arraylistviewandadapter/MainActivity.java
+++ app/src/main/java/com/example/arraylistviewandadapter/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.arraylistviewandadapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    EditText edt;
+    ListView lw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button00);
+        edt = findViewById(R.id.edittext00);
+        lw = findViewById(R.id.listview00);
+
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("Hassan");
+        ArrayAdapter<String> adp = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arr);
+        lw.setAdapter(adp);
+
+
+
     }
 }
